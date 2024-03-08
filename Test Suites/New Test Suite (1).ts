0988260a5c31b726a06c59daec682fee2c862f32
sabareesh <sabaree10krish@gmail.com>
2024-03-08 17:15:45 +0530

    dd

--- Test Suites/New Test Suite (1).ts
+++ Test Suites/New Test Suite (1).ts
@@ -12,6 +12,13 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>9f6c38c9-9ea5-4e91-8638-8316a5e4419f</testSuiteGuid>
    <testCaseLink>
+      <guid>f01aaeb2-fc49-4723-ba0a-7c5788a3d540</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
       <guid>3cac6b00-df96-4f41-83b1-42d273391e2e</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
@@ -19,7 +26,7 @@
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>f01aaeb2-fc49-4723-ba0a-7c5788a3d540</guid>
+      <guid>7c8b8817-3cb3-43f6-b16e-84c9210b8a10</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/New Test Case</testCaseId>
